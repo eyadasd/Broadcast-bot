9bcb376d0e49215dd1ae7531c4cc42068e083245
kij <oxoxw2x@gmail.com>
2018-12-22 15:57:43 +0300

    1.0

---
+++ commands/broadcast.js
@@ -0,0 +1,38 @@
+const Discord           = require('discord.js');
+const config            = require('./config.json');
+const prefix            = config.local.prefix;
+
+const log = (msg) => { console.log(msg) }
+
+module.exports = {
+    help: {
+        name:         "broadcast",
+        aliases:      ['bc']
+    },
+    run: async (bot, msg, args) => {
+        if(!msg.member.hasPermission('ADMINISTRATOR')) return bot.error(msg.channel, "Whoops!", "You don't meet the requirements to use this command!")
+        let message     = args.join(' ');
+        if(!message) return bot.error(msg.channel, "Invaild arguments!", `Please type the message you want to broadcast after the command.`);
+        if(message.length < 2000) return bot.erorr(msg.channel, "Whoops!", 'Message length cannot be longer than 2000 characters.');
+        
+        let a = 0;
+        let b = 0;
+        let c = 0;
+        let m = `Successfully sent to ${a}, failed to sent to ${b}, totally ${c}`;
+        msg.channel.send(m).then(m2 => {
+            msg.guild.members.forEach(member => {
+                c++;
+                if(!member.user.bot) {
+                    member.send(message).then(() => {
+                        a++;
+                    }).catch(e => { b++ });
+                } else b++;
+                if(c == Math.round(msg.guild.memberCount).toFixed() / 8) m2.edit(m);
+                else if(c == Math.round(msg.guild.memberCount).toFixed() / 5) m2.edit(m);
+                else if(c == Math.round(msg.guild.memberCount).toFixed() / 3) m2.edit(m);
+                else if(c == Math.round(msg.guild.memberCount).toFixed() / 2) m2.edit(m);
+                else if(c == Math.round(msg.guild.memberCount).toFixed()) m2.edit(`__**Done!**__\n` + m);
+            });
+        });
+    }
+}
---
+++ index.js
@@ -0,0 +1,78 @@
+/*
+ *
+ *
+ * Golden Developers (c) 2018-2019
+ * Made by: ImKIJ (Staff member)
+ * 
+ *  
+ */
+
+
+
+const Discord           = require('discord.js');
+const config            = require('./config.json');
+const bot               = new Discord.Client();
+const prefix            = config.local.prefix;
+
+const log = (msg) => { console.log(msg) }
+
+bot.login(process.env.TOKEN);
+
+bot.on(`ready`, async () => {
+    log(`Starting..`)
+    log(`${bot.user.username} is now working..`)
+    log(`Version: ${config.discord.version}`);
+});
+
+bot.error = function(channel, title, text) {
+    let embed = new Discord.RichEmbed();
+    embed.setColor(`#e8563c`);
+    embed.setTimestamp();
+    embed.setTitle(title);
+    embed.setDescription(text);
+    channel.send(embed);
+}
+
+bot.commands        = new Discord.Collection();
+bot.aliases         = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+    if (err) console.error(err);
+    if(!files) return;
+    log(`Loading a total of ${files.length} commands.`);
+    files.forEach(f => {
+        if(!f.endsWith(".js")) return;
+        let props = require(`./commands/${f}`);
+        log(`Attempting to load ${props.help.name}`);
+        bot.commands.set(props.help.name, props);
+        props.help.aliases.forEach(alias => {
+        bot.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+bot.on(`message`, async msg => {
+    if(!msg.channel.guild || msg.author.bot) return;
+    if(msg.content.indexOf(prefix) !== 0) return;
+
+    let args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+
+    let command;
+
+    if(bot.commands.has(cmd)){
+        command = bot.commands.get(cmd);
+    } else if (bot.aliases.has(cmd)){
+        command = bot.commands.get(bot.aliases.get(cmd));
+    }
+
+    if(command){
+        cmd.run(bot, msg, args);
+    }
+});
+
+bot.on('ready', async () => {
+    log('Logging in...')
+    log(`Logged in as ${bot.user.tag}`)
+    log(`Guilds Size: (${bot.guilds.size})`)
+});
